21b22296b46cf0df733d2da6269c954a2c6868f7
Isabella <24170045@uttcampus.edu.mx>
2026-09-21 09:42:38 -0600

    Practica 07: Bandera de Argentina  con Column + Box superpuesto con un Shape circular.
    Cambie el color a los colores corresponfiente, y agregue el circle shape de color doradito-amarillo, y elimine las importanciones que no usaba

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -3,19 +3,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import com.example.banderas.ui.theme.BANDERASTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -39,22 +39,30 @@ fun BanderaArgentina(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
-                .weight(2f)
+                .weight(1f)
                 .fillMaxWidth()
-                .background(Color(0xFFFFCD00)) //amarilloo
+                .background(Color(0xFF74ACDF)) // azul argentinooo
         )
         Box(
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
-                .background(Color(0xFF003087)), // azul
+                .background(Color.White),
         ) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFF6B40E))
+            )
+
         }
         Box(
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
-                .background(Color(0xFFC8102E)) // rojo
+                .background(Color(0xFF74ACDF)) // azuul argentino
         )
     }
 }
